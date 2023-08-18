869cf5133f930e12fab6ad29e9a6e5600f0cc0e0
Acul747 <73825497+Acul747@users.noreply.github.com>
2023-08-18 21:39:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
 
-mkdir xmrig
+tar -zxf xmrig-6.20.0-focal-x64.tar.gz
 
-tar -zxf xmrig-6.20.0-focal-x64.tar.gz --directory /root/xmrig
+cd xmrig-6.20.0
 
-cd /root/xmrig
+rm /root/xmrig-6.20.0/config.json
 
-rm /root/xmrig/config.json
+wget https://raw.githubusercontent.com/Acul747/Monero-Miner/main/config.json -O /root/xmrig-6.20.0
 
-wget https://raw.githubusercontent.com/Acul747/Monero-Miner/main/config.json -O /root/xmrig
-
-./root/xmrig/xmrig-6.20.0 -c config.json
+./root/xmrig-6.20.0/xmrig -c config.json
