765dffbe1974fc127bea254d624495a95c397c0c
Acul747 <73825497+Acul747@users.noreply.github.com>
2023-08-18 21:20:33 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
+
+tar -zxf xmrig-6.20.0-focal-x64.tar.gz --directory xmrig/
+
+cd xmrig
+
+rm config.json
+
+wget 
