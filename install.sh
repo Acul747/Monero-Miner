0620c234823cc1a4e95cd10065615d985a01d222
Acul747 <73825497+Acul747@users.noreply.github.com>
2023-08-18 21:24:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
 
-tar -zxf xmrig-6.20.0-focal-x64.tar.gz --directory xmrig/
+mkdir xmrig
+
+tar -zxf xmrig-6.20.0-focal-x64.tar.gz --directory xmrig
 
 cd xmrig
 
