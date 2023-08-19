5f30c94874e2fb24a67c71a48e73d1fadb4e6264
Acul747 <73825497+Acul747@users.noreply.github.com>
2023-08-19 19:14:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+pass="$1"
+config_file="config.json"
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
 
 tar -zxf xmrig-6.20.0-focal-x64.tar.gz
@@ -9,5 +12,7 @@ cd xmrig-6.20.0
 rm /root/xmrig-6.20.0/config.json
 
 wget https://raw.githubusercontent.com/Acul747/Monero-Miner/main/config.json -O /root/xmrig-6.20.0/config.json
+
+sed -i "s/\"pass\": \".*\"/\"pass\": \"$pass\"/g" "$config_file"
 
 /root/xmrig-6.20.0/xmrig -c /root/xmrig-6.20.0/config.json
